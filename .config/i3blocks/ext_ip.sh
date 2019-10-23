693a5b00c0a4e5c80fa656ab21a5e424c7163578
Caleb <c@leb.email>
2019-10-22 21:05:08 -0400

    Move coordinates to /tmp.

--- .config/i3blocks/ext_ip.sh
+++ .config/i3blocks/ext_ip.sh
@@ -6,7 +6,7 @@ if [[ "$ROUTE_EXISTS" = 'true' ]]; then
     IP=$(curl -s 'https://api.ipify.org')
     JSON=$(curl -s "https://ipapi.co/$IP/json")
     GEO=$(echo "$JSON" | jq -r '.city, .region_code, .country' | paste -sd '\ ' -)
-    echo "$JSON" | jq -r '.latitude, .longitude' | paste -sd ':' - > "$HOME/.config/i3blocks/coord.txt"
+    echo "$JSON" | jq -r '.latitude, .longitude' | paste -sd ':' - > "/tmp/coord.txt"
     echo "$GEO $IP"
     echo "$GEO $IP"
 else
