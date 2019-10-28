3ad7adfe000b7794aed8300a3e50e8cfb888aa6c
Caleb <c@leb.email>
2019-10-28 09:05:09 -0400

    Organize config by OS.

--- .bashrc.d/04-config.sh
+++ .bashrc.d/04-config.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-alias vv="vim $HOME/.vimrc"
-alias vb="vim $HOME/.bashrc"
-alias vbp="vim $HOME/.bash_profile"
 alias sb="source $HOME/.bash_profile"
+alias vb="vim $HOME/.bashrc"
 alias vbh="vim $HOME/.bash_history"
+alias vsc="vim $HOME/.ssh/config"
+alias vv="vim $HOME/.vimrc"
 tbh() { tail -n $( if [[ -z "$1" ]]; then echo 10; else echo "$1"; fi ) "$HOME/.bash_history"; }
-gbh() { g "$@" "$HOME/.bash_history"; }
-gbr() { g "$@" "$HOME/.bashrc"; }
+gbh() { grep -iE --color "$@" "$HOME/.bash_history"; }
+gbr() { grep -iE --color "$@" "$HOME/.bashrc"; }
 
-if [[ "$OSTYPE" == 'darwin'* ]]; then
-    alias vc="vim $HOME/.chunkwmrc"
-    alias vy="vim $HOME/.yabairc"
-    alias vs="vim $HOME/.skhdrc"
-    alias vk="vim $HOME/.config/kitty/kitty.conf"
-fi
+case "$OSTYPE" in
+    'darwin'*)
+        alias vc="vim $HOME/.chunkwmrc"
+        alias vy="vim $HOME/.yabairc"
+        alias vs="vim $HOME/.skhdrc"
+        alias vk="vim $HOME/.config/kitty/kitty.conf"
+        ;;
+    'linux-gnu')
+        alias van="vim $DROPBOX/Code/guides/arch_notes.txt"
+        alias vi3="vim $HOME/.config/i3/config"
+        alias vi3b="vim $HOME/.config/i3blocks/config"
+        alias vx="vim $HOME/.Xresources"
+        ;;
+esac
