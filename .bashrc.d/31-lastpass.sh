2c1ce76abc2d9c3ddf61706d21ebfebee14fb0cf
Caleb <c@leb.email>
2019-10-28 09:11:48 -0400

    Alias to duplicate a LastPass entry.

--- .bashrc.d/31-lastpass.sh
+++ .bashrc.d/31-lastpass.sh
@@ -4,5 +4,10 @@ alias lps='lpass show -Gx'
 alias lpe='lpass edit'
 alias lpt='lpass status'
 alias lpa='lpass add'
+lpc() {
+  lps "$1" | grep -E "^(Username|Password|URL):" | \
+    lpass add --non-interactive "$2"
+  lps "$2"
+}
 
 pwg() { LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 32 ; echo; }
