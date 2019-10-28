5a70110984d4f7a75f379f2e8c3d58d6265a9603
Caleb <c@leb.email>
2019-10-28 09:07:32 -0400

    Add pacman.

--- .bashrc.d/33-brew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-alias bs='brew search'
-alias bi='brew install'
-alias bci='brew cask install'
-alias bsl='brew services list'
-alias bsr='brew services restart'
---
+++ .bashrc.d/33-package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+case "$OSTYPE" in
+    'darwin'*)
+        alias bs='brew search'
+        alias bi='brew install'
+        alias bci='brew cask install'
+        alias bsl='brew services list'
+        alias bsr='brew services restart'
+        ;;
+    'linux-gnu')
+        alias pss="pacman -Ss"
+        alias pmi="sudo pacman -S"
+        alias cs="cower --sort votes -c -s"
+        alias ci="cower -i"
+        ;;
+esac
