7af2086df950c49f9552e3a17e298189e28612aa
Caleb <c@leb.email>
2019-10-28 09:11:07 -0400

    Add ss/netctl aliases.

--- .bashrc.d/21-network.sh
+++ .bashrc.d/21-network.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == 'linux-gnu' ]]; then
-    alias sn="sudo ss -antup | tail -n +2 | awk '{print \$1,\$2,\$5,\$6,\$7}' | sed -E 's/users:\(\(\"|,fd=.*//g ; s/\",pid=/\//g' | column -t | sort -k 5 | cut -c -\$COLUMNS"
-    alias sne="sn | grep 'ESTAB' | column -t"
-    alias snl="sn | grep -vE 'ESTAB|CLOSE-WAIT|LAST-ACK|TIME-WAIT|SYN-SENT' | column -t"
-    alias snu="sn | grep '^udp' | column -t"
+    # Socket statistics.
+    alias snb="{ ss -Hantup4; ss -Hantup6; } | awk '{print \$1,\$2,\$5,\$6,\$7}' | sed -E 's/users:\(\(\"|,fd=.*//g ; s/\",pid=/\//g' | column -t | sort -k 5"
+    alias sn="snb | cut -c -$COLUMNS"
+    alias sne="snb | grep 'ESTAB' | column -t | cut -c -$COLUMNS"
+    alias snl="snb | vg 'ESTAB|CLOSE-WAIT|LAST-ACK|TIME-WAIT|SYN-SENT|FIN-WAIT' | column -t | cut -c -$COLUMNS"
+    alias snu="snb | grep '^udp' | column -t | cut -c -$COLUMNS"
+
+    alias netctl='sudo /usr/bin/netctl'
+    alias nsa='netctl stop-all'
+    alias ns='netctl start'
+    nsn() { nsa; ns "$(ncl | head -n $1 | tail -n 1 | awk '{print $NF}')"; }
+    alias ncl='/usr/bin/netctl list | grep -n "" | column -t -s :'
+
+    pgw() { ping `ip route | awk '$1 == "default" {print $3}'`; }
 fi
 
 case "$OSTYPE" in 
@@ -21,3 +31,4 @@ alias png='ping google.com'
 arping() { "$(which arping)" -c 1 "$@" | g 'bytes from' || echo "No reply from $@."; }
 geo() { curl -s "https://ipapi.co/$@/json/"; }
 alias c='curl -skA "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0"'
+syn() { sudo nmap -oG - -Pn -sS -p "$2" "$1" | grep -oE 'Ports:.*'; }
