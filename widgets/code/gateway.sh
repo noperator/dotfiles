1366f64498034dc55813092236374ac8d22fcd6d
Caleb <c@leb.email>
2019-11-18 17:00:59 -0500

    Pad MAC address octets with zeroes.

--- widgets/code/gateway.sh
+++ widgets/code/gateway.sh
@@ -10,7 +10,7 @@ if [[ -z "$GATEWAY" ]]; then
     echo 'NONE'
 else
     if ! [[ -z "$GATEWAY" ]]; then
-        MAC=$(arp -an | awk "/\($GATEWAY\)/ {print \$4}" | sed -E 's/^(([0-9a-f]{1,2}:){3}).*/\1/' | tr -d ':')
+        MAC=$(arp -an | awk "/\($GATEWAY\)/ {print \$4}" | sed -E 's/^(([0-9a-f]{1,2}:){3}).*/\1/; s/(^|:)([0-9a-f])($|:)/\10\2\3/g' | tr -d ':')
         VENDOR=$(grep -i "$MAC" "$DIRNAME/oui.txt" | cut -f 3- | sed 's/,.*//' | awk '{print $1}')
         # VENDOR=$(grep -i "$MAC" '/usr/local/share/nmap/nmap-mac-prefixes' | cut -d ' ' -f 2- | sed 's/,.*//')
     fi
