f2db2f2225605506e6ddc475a7c7036f45a7c09d
Caleb <c@leb.email>
2019-12-05 16:05:34 -0500

    Remove commas from organization info.

--- widgets/code/geo.sh
+++ widgets/code/geo.sh
@@ -7,7 +7,7 @@ if [[ $((NOW - OLD_TIME)) -ge 120 ]]; then
     IP=$(curl -s 'https://api.ipify.org?format=json' | /usr/local/bin/jq -r '.ip')
     IPAPI=$(curl -s "https://ipapi.co/$IP/json/")
     GEO=$(echo "$IPAPI" | /usr/local/bin/jq -r '.city, .region_code' | tr '\n' '\ ')
-    ORG=$(echo "$IPAPI" | /usr/local/bin/jq -r '.org' | awk '{print $1}')
+    ORG=$(echo "$IPAPI" | /usr/local/bin/jq -r '.org' | awk '{print $1}' | tr -d ',')
     echo "@globe@ $IP $GEO $ORG" > "/var/tmp/geo_data.txt"
     date +%s > '/var/tmp/geo_time.txt'
 fi
