688ff6ba6d15b8fbc98b0533007bde81fd579baf
Caleb <c@leb.email>
2019-12-05 16:05:14 -0500

    Use tabs, not space, as a delimiter.

--- .bashrc.d/32-git.sh
+++ .bashrc.d/32-git.sh
@@ -4,6 +4,6 @@ alias ga='git add'
 alias gc='git commit --message'
 alias gd='git diff'
 alias gs='git status'
-alias gcl='git config --list --show-origin | column -t'
+alias gcl="git config --list --show-origin | column -t -s \"$(printf '\t')\""
 alias gl='git log'
 alias glo='git log --oneline'
