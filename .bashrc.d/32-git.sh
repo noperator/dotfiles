db4c0376664ef761943d1832a54f884298dd2a6d
Caleb <c@leb.email>
2019-12-17 15:04:22 -0500

    Add Git branch shortcut.

--- .bashrc.d/32-git.sh
+++ .bashrc.d/32-git.sh
@@ -6,3 +6,4 @@ alias gd='git diff'
 alias gs='git status'
 alias gcl="git config --list --show-origin | column -t -s \"$(printf '\t')\""
 alias gl="git log --date=short --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s'"
+alias gb='git branch -a'
