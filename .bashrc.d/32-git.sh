df4e5165d6394d2347fc5ae75dafd56348777c03
Caleb <c@leb.email>
2019-12-17 15:03:32 -0500

    Update one-line Git log command.

--- .bashrc.d/32-git.sh
+++ .bashrc.d/32-git.sh
@@ -5,5 +5,4 @@ alias gc='git commit --message'
 alias gd='git diff'
 alias gs='git status'
 alias gcl="git config --list --show-origin | column -t -s \"$(printf '\t')\""
-alias gl='git log'
-alias glo='git log --oneline'
+alias gl="git log --date=short --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s'"
