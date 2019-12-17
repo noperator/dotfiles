caec9906ba5f2270e7b04406badfcdd3d26c4bd3
Caleb <c@leb.email>
2019-12-17 15:05:00 -0500

    Keep Git commit subjects short.

--- .bashrc.d/32-git.sh
+++ .bashrc.d/32-git.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 alias ga='git add'
-alias gc='git commit --message'
+gc () {
+    MESSAGE="$@"
+    LENGTH=$(echo -n "$MESSAGE" | wc -c | tr -d ' ')
+    if [[ "$LENGTH" -gt 50 ]]; then
+        echo "Subject is too long ($LENGTH characters). Let's get it down to 50."
+    else
+        git commit --message "$MESSAGE"
+    fi
+}
 alias gd='git diff'
 alias gs='git status'
 alias gcl="git config --list --show-origin | column -t -s \"$(printf '\t')\""
