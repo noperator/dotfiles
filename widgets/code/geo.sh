244087ff46d49602eb86cc0e3ef3204f53b2c55d
Caleb <c@leb.email>
2019-12-28 10:44:47 -0500

    Update IP/geolocation data more quickly
    This script updates IP/geolocation data on a two-minute interval. Before this fix, sometimes it'd take that whole two minutes before this data was updated. Now, it updates it immediately.

--- widgets/code/geo.sh
+++ widgets/code/geo.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-OLD_TIME=$(cat '/var/tmp/geo_time.txt')
+touch /var/tmp/geo_time.txt /var/tmp/geo_data.txt
+
+OLD_TIME=$(cat /var/tmp/geo_time.txt)
 NOW=$(date +%s)
 
-if [[ $((NOW - OLD_TIME)) -ge 120 ]]; then
+if [[ $(cat /var/tmp/geo_data.txt) == '@globe@' ]] || [[ $((NOW - OLD_TIME)) -ge 120 ]]; then
     IP=$(curl -s 'https://api.ipify.org?format=json' | /usr/local/bin/jq -r '.ip')
     IPAPI=$(curl -s "https://ipapi.co/$IP/json/")
     GEO=$(echo "$IPAPI" | /usr/local/bin/jq -r '.city, .region_code' | tr '\n' '\ ')
     ORG=$(echo "$IPAPI" | /usr/local/bin/jq -r '.org' | awk '{print $1}' | tr -d ',')
-    echo "@globe@ $IP $GEO $ORG" > "/var/tmp/geo_data.txt"
-    date +%s > '/var/tmp/geo_time.txt'
+    echo "@globe@ $IP $GEO $ORG" > /var/tmp/geo_data.txt
+    date +%s > /var/tmp/geo_time.txt
 fi
 
-cat '/var/tmp/geo_data.txt'
+cat /var/tmp/geo_data.txt
