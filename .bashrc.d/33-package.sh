f1fe1a83c6302d4efe68bdaa05f2d15d471badea
Caleb <c@leb.email>
2020-02-03 10:20:19 -0500

    Add shortcut for listing installed packages

--- .bashrc.d/33-package.sh
+++ .bashrc.d/33-package.sh
@@ -11,6 +11,12 @@ case "$OSTYPE" in
     'linux-gnu'*)
         alias pss='pacman -Ss'
         alias pi='sudo pacman -S'
+        lip ()
+        {
+            for PACKAGE in $(pacman -Qqe); do
+                grep "\[ALPM\] installed $PACKAGE" /var/log/pacman.log
+            done | sort | uniq | sed 's/\[ALPM\] installed //'
+        }
         alias aurs='auracle --sort=votes search'
         alias auri='auracle info'
         alias aurc='auracle clone'
