5cb2153bce8e01b083877d04612ff48ffec1875b
Caleb <c@leb.email>
2020-02-11 09:48:07 -0500

    Add dotfiles/bin/ to PATH

--- .bashrc.d/00-init.sh
+++ .bashrc.d/00-init.sh
@@ -8,6 +8,7 @@ export HISTCONTROL='ignoreboth'  # Shorthand for ignorespace and ignoredups.
 export DIALOGRC="$HOME/.dialogrc"
 export SSH_AUTH_SOCK="$HOME/.ssh/ssh-agent.$HOSTNAME.sock"
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
+export PATH="$HOME/dotfiles/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
 
 if [[ -z "$TMUX" ]]; then
     export INPUTRC="$HOME/.inputrc"
