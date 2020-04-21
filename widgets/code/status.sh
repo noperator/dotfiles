bbea97af60926f178d1a2c9ca827c3856c5b9f34
Caleb <c@leb.email>
2020-04-21 12:28:31 -0400

    Stop breaking status script output across lines

--- widgets/code/status.sh
+++ widgets/code/status.sh
@@ -4,8 +4,8 @@ DIRNAME=$(dirname "$0")
 
 FIRST='true'
 for SCRIPT in geo vpn gateway wifi battery date; do
-    [[ "$FIRST" == 'true' ]] || echo '|'
-    "$DIRNAME/${SCRIPT}.sh"
+    [[ "$FIRST" == 'true' ]] || echo -n ' | '
+    "$DIRNAME/${SCRIPT}.sh" | tr '\n' ' '
     FIRST='false'
 done
 
