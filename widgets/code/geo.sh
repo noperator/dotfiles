6e688baf9f7f29be58f88a1df5da10dd9a4e6d15
Caleb <c@leb.email>
2020-04-22 13:04:30 -0400

    Only geolocate after IP change (avoid rate limits)

--- widgets/code/geo.sh
+++ widgets/code/geo.sh
@@ -7,10 +7,13 @@ NOW=$(date +%s)
 
 if [[ $(cat /var/tmp/geo_data.txt) == '@globe@' ]] || [[ $((NOW - OLD_TIME)) -ge 120 ]]; then
     IP=$(curl -s 'https://api.ipify.org?format=json' | /usr/local/bin/jq -r '.ip')
-    IPAPI=$(curl -s "https://ipapi.co/$IP/json/")
-    GEO=$(echo "$IPAPI" | /usr/local/bin/jq -r '.city, .region_code' | tr '\n' '\ ')
-    ORG=$(echo "$IPAPI" | /usr/local/bin/jq -r '.org' | awk '{print $1}' | tr -d ',')
-    echo "@globe@ $IP $GEO $ORG" > /var/tmp/geo_data.txt
+    OLD_IP=$(awk '{print $2}' /var/tmp/geo_data.txt)
+    if [[ "$IP" != "$OLD_IP" ]]; then
+        IPAPI=$(curl -s "https://ipapi.co/$IP/json/")
+        GEO=$(echo "$IPAPI" | /usr/local/bin/jq -r '.city, .region_code' | tr '\n' '\ ')
+        ORG=$(echo "$IPAPI" | /usr/local/bin/jq -r '.org' | awk '{print $1}' | tr -d ',')
+        echo "@globe@ $IP $GEO $ORG" > /var/tmp/geo_data.txt
+    fi
     date +%s > /var/tmp/geo_time.txt
 fi
 
