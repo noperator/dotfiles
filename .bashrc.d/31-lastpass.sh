03a8e575db72d0780ea80641f0aaa7d8e4456737
Caleb <c@leb.email>
2020-05-04 12:02:45 -0400

    Enable lpass account switching

--- .bashrc.d/31-lastpass.sh
+++ .bashrc.d/31-lastpass.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+# Inspiration for lpass account switching:
+# https://github.com/lastpass/lastpass-cli/issues/445#issuecomment-522701216
+
+export LPASS_HOME="$HOME/.lpass-personal"
+alias lpt='lpass status; echo LPASS_HOME=$LPASS_HOME'
+
+# Create folders for separate LastPass accounts.
+for ACCOUNT in personal work; do
+    DIR="$HOME/.lpass-$ACCOUNT"
+    if ! [[ -d "$DIR" ]]; then
+        mkdir "$DIR"
+    fi
+done
+
+# Switch LastPass account.
+lpsa() {
+    DIR="$HOME/.lpass-$1"
+    if ! [[ -d "$DIR" ]]; then
+        echo "$DIR does not exist."
+    else
+        export LPASS_HOME="$HOME/.lpass-$1"
+        lpt
+    fi
+}
+
 alias lps='lpass show -Gx'
 alias lpe='lpass edit'
-alias lpt='lpass status'
 alias lpa='lpass add'
 lpc() {
   lps "$1" | grep -E "^(Username|Password|URL):" | \
