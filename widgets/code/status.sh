80ec83c31956b2bbd418509a480dd1e0ffb1b2f5
Caleb <c@leb.email>
2020-05-04 11:36:25 -0400

    Split script names across multiple lines

--- widgets/code/status.sh
+++ widgets/code/status.sh
@@ -3,9 +3,15 @@
 DIRNAME=$(dirname "$0")
 
 FIRST='true'
-for SCRIPT in geo vpn gateway wifi battery date; do
+for SCRIPT in \
+geo \
+vpn \
+gateway \
+wifi \
+battery \
+date \
+; do \
     [[ "$FIRST" == 'true' ]] || echo -n ' | '
     "$DIRNAME/${SCRIPT}.sh" | tr '\n' ' '
     FIRST='false'
 done
-
