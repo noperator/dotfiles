3c419dc9034f0d76d58f1588a3ecff703b44e6ba
Caleb <c@leb.email>
2020-05-19 10:45:23 -0400

    Ensure profile opened before opening the next one

--- .bashrc.d/37-browser.sh
+++ .bashrc.d/37-browser.sh
@@ -3,8 +3,19 @@
 if [[ "$OSTYPE" == 'darwin'* ]]; then
     fp()
     {
-        rm ~/Library/Application\ Support/Firefox/Profiles/*/.parentlock
+        # Prevent error message, "A copy of Firefox is already open. Only one copy of Firefox can be open at a time."
+        # More info: https://apple.stackexchange.com/a/238169
+        rm ~/Library/Application\ Support/Firefox/Profiles/*/.parentlock 2>/dev/null
         /Applications/Firefox.app/Contents/MacOS/firefox --no-remote -P "$@" &
     }
-    alias fpa='for PROFILE in Work Personal; do fp "$PROFILE"; sleep 1; done'
+    fpa()
+    {
+        # Open Work profile first so links are automatically opened there by default.
+        for PROFILE in Work Personal; do
+            fp "$PROFILE"
+            while ! [[ $(pgrep -fl "firefox.*-P $PROFILE") ]]; do
+                sleep 1
+            done
+        done
+    }
 fi
