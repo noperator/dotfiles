9c43b553923d826cb94b68279db29614784b8317
Caleb <c@leb.email>
2020-06-01 12:28:00 -0400

    Disable backspace as Firefox back navigation

--- firefox/user.js
+++ firefox/user.js
@@ -4,3 +4,6 @@ user_pref("browser.urlbar.matchBuckets", "general:5,suggestion:Infinity");
 // Disable back/forward swipe gestures.
 user_pref("browser.gesture.swipe.left", "");
 user_pref("browser.gesture.swipe.right", "");
+
+// Disable backspace as back navigation button.
+user_pref("browser.backspace_action", 2);
