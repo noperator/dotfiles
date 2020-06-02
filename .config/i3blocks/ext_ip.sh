15420ac329c4fd1b4a608a38b3bc07bc7c53c50a
Caleb <c@leb.email>
2020-06-02 11:12:21 -0400

    Use OpenDNS, not ipify, for i3blocks public IP

--- .config/i3blocks/ext_ip.sh
+++ .config/i3blocks/ext_ip.sh
@@ -3,7 +3,7 @@
 source "$HOME/.config/i3blocks/network.sh"
 
 if [[ "$ROUTE_EXISTS" = 'true' ]]; then
-    IP=$(curl -s 'https://api.ipify.org')
+    IP=$(dig -4 +short A myip.opendns.com @resolver1.opendns.com)
     JSON=$(curl -s "https://ipapi.co/$IP/json")
     GEO=$(echo "$JSON" | jq -r '.city, .region_code, .country' | paste -sd '\ ' -)
     echo "$JSON" | jq -r '.latitude, .longitude' | paste -sd ':' - > "/tmp/coord.txt"
