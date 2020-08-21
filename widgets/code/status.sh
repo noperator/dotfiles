9b603bb7b32bd728ad27bb04bfb2d4e08b97e89a
Caleb <c@leb.email>
2020-08-20 23:03:57 -0400

    Detect Slack screen sharing, in addition to Teams

--- widgets/code/status.sh
+++ widgets/code/status.sh
@@ -2,9 +2,22 @@
 
 DIRNAME=$(dirname "$0")
 
-# vpn \
+# Check if Slack or Teams are screen sharing.
+screen_sharing() {
+    if [[ $(/usr/local/bin/yabai -m query --windows |
+            jq -r '[.[] | select(.app      == "Slack" and
+                                 .floating == 1       and
+                                 .frame.w  == 400     and
+                                 .frame.h  == 56)] | length') -gt 0 ]] ||
+       pgrep -f '\-msteams-process-type=appSharingToolbar' &>/dev/null; then
+        echo '@exclamation-triangle@ @desktop@  '
+    else
+        false
+    fi
+}
 
-if [[ ! $(pgrep -afli 'appSharingToolbar') ]]; then
+# Only display networking information if not screen sharing.
+if ! screen_sharing; then
     for SCRIPT in \
     geo \
     gateway \
