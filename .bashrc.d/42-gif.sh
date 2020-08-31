81c028b491b09979ef88b28592acfaf7ab48f67b
Caleb <c@leb.email>
2020-08-31 16:16:11 -0400

    Remove old extension from GIF filename

--- .bashrc.d/42-gif.sh
+++ .bashrc.d/42-gif.sh
@@ -3,5 +3,5 @@
 gif() {
     ffmpeg -i "$@" -r 10 -f image2pipe -vcodec ppm - |
     convert -verbose +dither -layers Optimize -resize 700x700\> - gif:- |
-    gifsicle --colors 128 --delay=5 --loop --optimize=3 --multifile - > "${@}.gif"
+    gifsicle --colors 128 --delay=5 --loop --optimize=3 --multifile - > "${1%.*}.gif"
 }
