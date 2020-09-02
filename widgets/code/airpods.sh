22b8b91b3f410b839b853412d5fcfb71c3f03947
Caleb <c@leb.email>
2020-09-02 14:13:07 -0400

    Don't print Airpods case battery when disconnected

--- widgets/code/airpods.sh
+++ widgets/code/airpods.sh
@@ -4,8 +4,6 @@
 # - https://gist.github.com/Jahhein/45b8e8c9c36a0932189a5037f990bcdd
 
 BT_DEFAULTS=$(defaults read /Library/Preferences/com.apple.Bluetooth)
-
-# MAC_ADDR=$(system_profiler SPBluetoothDataType 2>/dev/null | grep -A 1 'AirPods Pro' | awk '/Address:/ {print $2}')
 CONNECTED=$(
     system_profiler SPBluetoothDataType 2>/dev/null |
     grep -E '(AirPods Pro|Connected):' |
@@ -13,24 +11,13 @@ CONNECTED=$(
     awk '/Connected:/ {print $2}'
 )
 
-echo '@headphones@'
 if [[ "$CONNECTED" == 'Yes' ]]; then
-    for EAR in Left Right; do
-        <<< "$BT_DEFAULTS" awk "/BatteryPercent$EAR/"' {sub(";", "%", $3); print $3}'
-
-        # PERCENT=$(<<< "$BT_DEFAULTS" awk "/BatteryPercent$EAR/"' {sub(";", "", $3); print $3}')
-        # if   [[ "$PERCENT" -lt 10 ]]; then
-        #     LVL='empty'
-        # elif [[ "$PERCENT" -lt 35 ]]; then
-        #     LVL='quarter'
-        # elif [[ "$PERCENT" -lt 65 ]]; then
-        #     LVL='half'
-        # elif [[ "$PERCENT" -lt 90 ]]; then
-        #     LVL='three-quarters'
-        # else
-        #     LVL='full'
-        # fi
-        # echo "@battery-$LVL@ "
-
+    echo '@headphones@'
+    for PART in Left Right Case; do
+        <<< "$BT_DEFAULTS" awk -v "part=$PART" "/BatteryPercent$PART/"' {
+            sub(";", "%", $3);
+            if (!(part == "Case" && $3 == "0%"))  # Case disconnected.
+                print $3;
+        }'
     done
 fi
