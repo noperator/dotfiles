8058af5b3df238ac495d0514b591c56da9776bfb
Caleb <c@leb.email>
2020-09-04 11:20:07 -0400

    Don't print battery status when fully charged

--- widgets/code/battery.sh
+++ widgets/code/battery.sh
@@ -4,6 +4,9 @@ BATTERY=$(pmset -g batt)
 PERCENT=$(<<< "$BATTERY" grep -oE '\d*%' | tr -d '%')
 SOURCE=$(<<< "$BATTERY" tr -d "'" | awk '/Now drawing from/ {print $4}')
 
+# Don't bother printing battery status if it's fully charged.
+[[ "$PERCENT" -eq '100' ]] && exit
+
 if [[ "$SOURCE" == 'AC' ]]; then
     ICON='plug'
 else
