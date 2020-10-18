a9b39080a04b46acc5348f1e744ceddcb0283885
Caleb <c@leb.email>
2020-10-18 11:50:56 -0400

    Clean up logic for spawning URxvt windows

--- .config/i3/cwd.sh
+++ .config/i3/cwd.sh
@@ -1,10 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-DEST_WD="$HOME"
-ACTIVE_ID=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print $NF}')
-ACTIVE_TITLE=$(xprop -id "$ACTIVE_ID" WM_COMMAND)
-if [[ "$ACTIVE_TITLE" = *"$TERMINAL"* ]]; then
-  ACTIVE_PID=$(xprop -id "$ACTIVE_ID" _NET_WM_PID | awk '{print $NF}')
-  DEST_WD=$(readlink -e /proc/`pgrep --parent "$ACTIVE_PID" ${SHELL##*/}`/cwd)
+# Spawn a new URxvt terminal window. If the active (i.e., focused) window is a
+# URxvt terminal window, find the current working directory of its
+# corresponding attached shell, and spawn a new terminal window in the same
+# working directory. Otherwise, spawn the shell in the home directory.
+ACTIVE_WIN_ID=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print $NF}')
+ACTIVE_WIN_CLASS=$(xprop -id "$ACTIVE_WIN_ID" WM_CLASS | awk '{gsub(/"/, "", $NF); print $NF}')
+if [[ "$ACTIVE_WIN_CLASS" == 'URxvt' ]]; then
+    # # and % delete the shortest possible matching string, and ## and %%
+    # delete the longest possible.
+    # - https://stackoverflow.com/a/15988793
+    TERM_PID=$(xprop -id "$ACTIVE_WIN_ID" _NET_WM_PID | awk '{print $NF}')
+    SHELL_PID=$(pgrep -P "$TERM_PID" ${SHELL##*/})
+    DEST_WD=$(readlink -e "/proc/$SHELL_PID/cwd")
+else
+    DEST_WD="$HOME"
 fi
+
 urxvt -cd "$DEST_WD"
