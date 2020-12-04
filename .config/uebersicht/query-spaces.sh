4321de30976cb6a1ba25807742b0ccc2784bd7a0
Caleb <c@leb.email>
2020-12-04 16:10:45 -0500

    Don't show minimized windows in spaces bar

--- .config/uebersicht/query-spaces.sh
+++ .config/uebersicht/query-spaces.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 # Use yabai to query all spaces while filtering each space's list of windows
-# to ignore hidden Microsoft Teams windows.
+# to ignore windows having certain characteristics.
 
 # Find yabai.
 PATH="/usr/local/bin:$PATH"
 
-# Get window IDs of hidden Teams windows.
+# Get window IDs of hidden Teams windows and minimized windows.
 TEAMS_WINDOWS=$(yabai -m query --windows |
-    jq '[.[] | select(.title | test("^Microsoft Teams (Notification|Call)")) | .id] | unique')
+    jq '[.[] | select(
+        (.title | test("^Microsoft Teams (Notification|Call)|Reminders?$"))
+        or
+        .minimized == 1
+        ) | .id] | unique')
 [[ -z "$TEAMS_WINDOWS" ]] && TEAMS_WINDOWS='[]'
 
 # Get spaces while removing windows whose IDs match Teams windows.
