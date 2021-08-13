0cfe9cffa2b540270340126f3f3101dc72b09173
Caleb <c@leb.email>
2021-08-13 10:43:23 -0400

    Edit IPython in vi mode

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -569,7 +569,7 @@
 
 ## Shortcut style to use at the prompt. 'vi' or 'emacs'.
 #  Default: 'emacs'
-# c.TerminalInteractiveShell.editing_mode = 'emacs'
+c.TerminalInteractiveShell.editing_mode = 'vi'
 
 ## Set the editor used by IPython (default to $EDITOR/vi/notepad).
 #  Default: 'vim'
