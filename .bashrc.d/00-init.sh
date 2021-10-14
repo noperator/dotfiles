6d599940eebb91a834253092e07bc5c97132d3ab
Caleb <c@leb.email>
2021-10-14 12:44:40 -0400

    Only show elapsed time for `time`
    Also various `shfmt` formatting changes.

--- .bashrc.d/00-init.sh
+++ .bashrc.d/00-init.sh
@@ -2,34 +2,34 @@
 
 export VISUAL='vim'
 export EDITOR="$VISUAL"
-export HISTCONTROL='ignoreboth'  # Shorthand for ignorespace and ignoredups.
+export HISTCONTROL='ignoreboth' # Shorthand for ignorespace and ignoredups.
 export DIALOGRC="$HOME/.dialogrc"
 export SSH_AUTH_SOCK="$HOME/.ssh/ssh-agent.$HOSTNAME.sock"
 export TMPDIR='/tmp/'
 export NPM_PACKAGES="$HOME/.npm-packages"
-tabs -4  # Set tab interval.
+export TIMEFORMAT='%3lR' # Only show elapsed time.
+tabs -4                  # Set tab interval.
 
 # Prepend directories to PATH without the variable growing each time this file is sourced.
 OLD_PATH=$(echo "$PATH" | tr ':' '\n' | uniq | tr '\n' ':')
 CUSTOM_PATH=''
 for DIR in \
-"$HOME/dotfiles/bin" \
-"$HOME/.local/bin" \
-"$NPM_PACKAGES/bin" \
-"$HOME/go/bin" \
-"$HOME/.cargo/bin" \
-/usr/local/go/bin \
-/usr/local/Cellar/python\@3.9/3.9.0/bin \
-/snap/bin \
-/usr/local/opt/util-linux/sbin \
-/usr/local/opt/util-linux/bin \
-/usr/local/sbin \
-/usr/local/bin \
-/usr/sbin \
-/usr/bin \
-/sbin \
-/bin \
-; do
+    "$HOME/dotfiles/bin" \
+    "$HOME/.local/bin" \
+    "$NPM_PACKAGES/bin" \
+    "$HOME/go/bin" \
+    "$HOME/.cargo/bin" \
+    /usr/local/go/bin \
+    /usr/local/Cellar/python\@3.9/3.9.0/bin \
+    /snap/bin \
+    /usr/local/opt/util-linux/sbin \
+    /usr/local/opt/util-linux/bin \
+    /usr/local/sbin \
+    /usr/local/bin \
+    /usr/sbin \
+    /usr/bin \
+    /sbin \
+    /bin; do
     OLD_PATH=$(echo "$OLD_PATH" | sed -E "s%(^|:)$DIR(:|$)%\1\2%")
     CUSTOM_PATH="$CUSTOM_PATH:$DIR"
 done
