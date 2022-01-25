ed9bd820cb1e9aa73b3e510d9448fef6209074c5
Caleb <c@leb.email>
2022-01-25 10:37:08 -0500

    Add Emoji Finder

--- .bashrc.d/10-misc.sh
+++ .bashrc.d/10-misc.sh
@@ -127,3 +127,21 @@ ran() {
     done
     echo
 }
+
+# Emoji Finder.
+ef() {
+    echo "https://emojifinder.com/$1"
+    curl -s "https://emojifinder.com/*/ajax.php?action=search&query=$1" |
+        jq -r '.results | sort_by(.display_mode) | .[] | [.display_code, .Name] | join("@")' |
+        while IFS=@ read CODE NAME; do
+            echo -e "$(sed <<<$CODE -E 's/&#x(.*);/\\U\1/')@$(tr <<<$NAME '[:upper:]' '[:lower:]')"
+        done |
+        column -t -s '@'
+    # while IFS=@ read CODES NAME; do
+    #     echo $(tr <<<$NAME '[:upper:]' '[:lower:]')
+    #     echo $CODES | tr ';' '\n' | while read CODE; do
+    #         echo -n "$CODE "
+    #         echo -e "$(sed <<<$CODE -E 's/&#x(.*)/\\U\1/')"
+    #     done
+    # done
+}
