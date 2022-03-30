a6f7507e973c0338ca49958ed28ccba111facc9d
Caleb <c@leb.email>
2022-03-29 21:31:36 -0400

    Log in with SSO-enabled AWS profile

--- .bashrc.d/39-aws.sh
+++ .bashrc.d/39-aws.sh
@@ -7,9 +7,21 @@ get_ec2_instance_id() {
         --query 'Reservations[*].Instances[*].[InstanceId]' \
         --output text
 }
+
 control_ec2() {
     aws ec2 "$1-instances" \
         --profile instance-state-user \
         --instance-ids $(get_ec2_instance_id "$2") \
         --output text
+}
+
+# Log in with SSO-enabled AWS profile. Since we can't tell the aws tool which
+# browser profile we want it to open the SSO prompt in, we manually open the
+# URL using gco() defined in 37-browser.sh.
+# Usage:
+# sso_login <PROFILE> <BROWSER>
+sso_login() {
+    exec 3< <(aws sso login --profile "$1" --no-browser | tee /dev/tty)
+    sleep 1
+    gco -d "$HOME/.config/chrome-$2" -u $(grep -m 1 user_code <&3)
 }
