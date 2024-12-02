759483faeee2c47d175ddcc99eb7db30ffd23aae
noperator <noper@tor.fyi>
2024-12-02 14:13:32 -0500

    alacritty stuff

--- .config/alacritty/launch-instance.sh
+++ .config/alacritty/launch-instance.sh
@@ -1,34 +1,70 @@
 #!/bin/bash
 
-# Spawn a new Alacritty terminal window. If the active (i.e., focused) window
-# is a Alacritty terminal window, find the current working directory of its
-# corresponding attached shell, and spawn a new terminal window in the same
-# working directory. Otherwise, spawn the shell in the home directory.
+# Spawn a new Alacritty terminal window with smart display placement based on
+# cursor location and window focus.
 DEST_WD="$HOME"
 OPTS=''
+
 case "$OSTYPE" in
 'darwin'*)
     PATH="/Applications/Alacritty.app/Contents/MacOS/:$PATH"
-    yabai -m signal --add \
-        action="yabai -m signal --remove temp_move_alacritty;
-                yabai -m window --focus \$YABAI_WINDOW_ID;" \
-        app=Alacritty \
-        event=window_created \
-        label=temp_move_alacritty
+
+    # Get display info before spawning
+    CURSOR_DISPLAY=$(yabai -m query --displays --display mouse | jq '.index')
+    FOCUSED_WINDOW=$(yabai -m query --windows --window)
+    WINDOW_DISPLAY=$(echo "$FOCUSED_WINDOW" | jq '.display')
+    IS_ALACRITTY=$(echo "$FOCUSED_WINDOW" | jq '.app == "Alacritty"')
+    # {
+    #     echo "Cursor display: $CURSOR_DISPLAY"
+    #     echo "Focused window display: $WINDOW_DISPLAY"
+    #     echo "Is focused window Alacritty: $IS_ALACRITTY"
+    # } >/tmp/out
+
     if pgrep -aq alacritty; then
         OPTS='msg create-window'
-        TERM_PID=$(yabai -m query --windows | jq '.[] | select(.["has-focus"] == true and .app == "Alacritty") | .pid')
-        # SHELL_INDEX=$(yabai -m query --windows | jq 'map(select(.app == "Alacritty") | {id, "has-focus"}) | sort_by(.id) | to_entries[] | select(.value | .["has-focus"] == true) | .key')
-        if [[ -n "$TERM_PID" ]]; then
-            # SHELL_PID=$(pgrep -aP "$TERM_PID" sh | sort -n | sed "$(($SHELL_INDEX + 1))q;d")
-            # DEST_WD=$(lsof -a -p "$SHELL_PID" -d cwd -F n | tail -n 1 | sed -E 's/^.//')
-            DEST_WD=$(yabai -m query --windows | jq -r '.[] | select(.["has-focus"] == true and .app == "Alacritty") | .title')
+        if [[ "$IS_ALACRITTY" == "true" ]]; then
+            # Get working directory from focused terminal
+            DEST_WD=$(echo "$FOCUSED_WINDOW" | jq -r '.title')
             if ! [[ -d "$DEST_WD" ]]; then
                 DEST_WD="$HOME"
             fi
         fi
+
+        # Set up signal to handle the new window
+        # if [[ "$CURSOR_DISPLAY" != "$WINDOW_DISPLAY" ]]; then
+        #     # Cursor and window on different displays - move to cursor's display after creation
+        #     yabai -m signal --add \
+        #         action="yabai -m window --display $WINDOW_DISPLAY; \
+        #                 yabai -m window --focus \$YABAI_WINDOW_ID; \
+        #                 yabai -m window --display $CURSOR_DISPLAY; \
+        #                 yabai -m signal --remove temp_move_alacritty" \
+        #         app=Alacritty \
+        #         event=window_created \
+        #         label=temp_move_alacritty
+        # else
+        #     # Cursor and window on same display - just focus
+        #     yabai -m signal --add \
+        #         action="yabai -m window --display $WINDOW_DISPLAY; \
+        #                 yabai -m window --focus \$YABAI_WINDOW_ID; \
+        #                 yabai -m signal --remove temp_move_alacritty" \
+        #         app=Alacritty \
+        #         event=window_created \
+        #         label=temp_move_alacritty
+        # fi
     fi
+
+    # if [[ "$IS_ALACRITTY" == "false" ]]; then
+    yabai -m signal --add \
+        action="yabai -m window --focus \$YABAI_WINDOW_ID; \
+                    yabai -m window --display $CURSOR_DISPLAY; \
+                    yabai -m window --focus \$YABAI_WINDOW_ID; \
+                    yabai -m signal --remove temp_move_alacritty" \
+        app=Alacritty \
+        event=window_created \
+        label=temp_move_alacritty
+    # fi
     ;;
+
 'linux-gnu'*)
     PATH="$HOME/.cargo/bin:$PATH"
     ACTIVE_WIN_ID=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print $NF}')
@@ -41,9 +77,4 @@ case "$OSTYPE" in
     ;;
 esac
 
-# echo "term:  $TERM_PID"
-# echo "shell: $SHELL_PID"
-# echo "wd:    $DEST_WD"
-# OPTS="$OPTS --working-directory $DEST_WD"
-echo "$OPTS"
 alacritty $OPTS --working-directory "$DEST_WD"
